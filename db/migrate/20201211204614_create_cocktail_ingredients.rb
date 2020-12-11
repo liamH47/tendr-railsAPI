6e518191aaa0fe90298beb65ebd5c6a2d277f08c
liamH47 <liamhanafee@gmail.com>
2020-12-11 16:00:09 -0500

    db tables ready, relationships next

--- db/migrate/20201211204614_create_cocktail_ingredients.rb
+++ db/migrate/20201211204614_create_cocktail_ingredients.rb
@@ -1,7 +1,12 @@
 class CreateCocktailIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :cocktail_ingredients do |t|
-
+      t.integer :ingredient_id
+      t.integer :cocktail_id 
+      t.string :name 
+      t.string :category
+      t.string :image_url
+      t.string :measurement
       t.timestamps
     end
   end
