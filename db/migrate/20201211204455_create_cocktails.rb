903fc21f90ec53825d842cc6a6f822ff6aceda71
liamH47 <liamhanafee@gmail.com>
2020-12-12 22:40:48 -0500

    more table tweeks, seeds almost done

--- db/migrate/20201211204455_create_cocktails.rb
+++ db/migrate/20201211204455_create_cocktails.rb
@@ -2,6 +2,7 @@ class CreateCocktails < ActiveRecord::Migration[6.0]
   def change
     create_table :cocktails do |t|
       t.string :name
+      t.string :image_url
       t.text :instructions, array: true, default: []
       t.string :recommended_glass
       t.string :recommended_ice
