e15e07cc2e198c667a2d834288481c00013ce370
liamH47 <liamhanafee@gmail.com>
2020-12-12 22:13:11 -0500

    fixed tables and began seeding

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,4 @@
 class Cocktail < ApplicationRecord
-    has_many :cocktail_ingredients, dependent: destroy
-    has_many :ingredients, through: cocktail_ingredients
+    has_many :cocktail_ingredients, dependent: :destroy
+    has_many :ingredients, through: :cocktail_ingredients
 end
