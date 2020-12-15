dd0917d27cf5056c1251c626787b908c67762755
liamH47 <liamhanafee@gmail.com>
2020-12-15 11:02:34 -0500

    delete working

--- app/controllers/api/v1/user_ingredients_controller.rb
+++ app/controllers/api/v1/user_ingredients_controller.rb
@@ -10,4 +10,8 @@ class Api::V1::UserIngredientsController < ApplicationController
         render json: @user_ingredients
     end
 
+    def destroy 
+        @user_ingredient = UserIngredient.find(params[:id])
+        @user_ingredient.destroy
+    end
 end
