c188cd889bc50426c8d98923b42cabc01bac89fe
liamH47 <liamhanafee@gmail.com>
2020-12-23 17:55:55 -0500

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,66 +12,135 @@
 # Cocktail.destroy_all
 
 
-user = User.create!(
-    name: "Liam",
-    email: "liam@gmail.com",
-    password: "1234"
-)
+# user = User.create!(
+#     name: "Liam",
+#     email: "liam@gmail.com",
+#     password: "1234"
+# )
 
-Ingredient.create!(
-    name: "White rum",
-    category: "spirit",
-    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ02-7PvD9VREpBCNSIKDzx75PvPhtIjzqz1w&usqp=CAU",
-    quantity: 34,
-    unit: "oz"
-)
+# Ingredient.create!(
+#     name: "White rum",
+#     category: "spirit",
+#     image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ02-7PvD9VREpBCNSIKDzx75PvPhtIjzqz1w&usqp=CAU",
+#     quantity: 34,
+#     unit: "oz"
+# )
 
-bourbon = Ingredient.create!(
-    name: "Bourbon",
-    category: "spirit",
-    image_url: "https://www.binnys.com/media/catalog/product/cache/eab16ae251e4410504af434c6d9419db/1/9/190665.jpg",
-    quantity: 34,
-    unit: "oz"
-)
+# bourbon = Ingredient.create!(
+#     name: "Bourbon",
+#     category: "spirit",
+#     image_url: "https://www.binnys.com/media/catalog/product/cache/eab16ae251e4410504af434c6d9419db/1/9/190665.jpg",
+#     quantity: 34,
+#     unit: "oz"
+# )
 
-ango = Ingredient.create!(
-    name: "Angostura Bitters",
-    category: "Bitter",
-    image_url: "https://cdn11.bigcommerce.com/s-7a906/images/stencil/1000x1000/products/10909/10165/Angostura-Bitters__91961.1486143395.jpg?c=2",
-    quantity: 160,
-    unit: "dashes"
-)
+# ango = Ingredient.create!(
+#     name: "Angostura Bitters",
+#     category: "Bitter",
+#     image_url: "https://cdn11.bigcommerce.com/s-7a906/images/stencil/1000x1000/products/10909/10165/Angostura-Bitters__91961.1486143395.jpg?c=2",
+#     quantity: 160,
+#     unit: "dashes"
+# )
 
-simple = Ingredient.create!(
-    name: "Simple Syrup",
-    category: "Sweetener",
-    image_url: "https://www.simplyrecipes.com/wp-content/uploads/2019/09/simple-syrup-lead-3-600x840.jpg",
-    quantity: 12,
-    unit: "oz"
-)
-
-lemon_juice = Ingredient.create!(
-    name: "Lemon Juice",
-    category: "Citrus Juice",
-    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8-FlXjdmsieEXlT28UOlnS-7DVXrr4mZw4g&usqp=CAU",
-    quantity: 12,
-    unit: "oz"
-)
-
-egg_white = Ingredient.create!(
-    name: "Egg White",
-    category: "Emulsifier",
-    image_url: "https://sc02.alicdn.com/kf/ULB8jd4bLNHEXKJk43Jeq6yeeXXaI.jpg",
-    quantity: 12,
-    unit: ""
-)
-# sugar_cube = Ingredient.create!(
-#     name: "Sugar Cubes",
+# simple = Ingredient.create!(
+#     name: "Simple Syrup",
 #     category: "Sweetener",
 #     image_url: "https://www.simplyrecipes.com/wp-content/uploads/2019/09/simple-syrup-lead-3-600x840.jpg",
 #     quantity: 12,
 #     unit: "oz"
 # )
+
+# lemon_juice = Ingredient.create!(
+#     name: "Lemon Juice",
+#     category: "Citrus Juice",
+#     image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8-FlXjdmsieEXlT28UOlnS-7DVXrr4mZw4g&usqp=CAU",
+#     quantity: 12,
+#     unit: "oz"
+# )
+
+# egg_white = Ingredient.create!(
+#     name: "Egg White",
+#     category: "Emulsifier",
+#     image_url: "https://sc02.alicdn.com/kf/ULB8jd4bLNHEXKJk43Jeq6yeeXXaI.jpg",
+#     quantity: 12,
+#     unit: ""
+# )
+# gin = Ingredient.create!(
+#     name: "Gin",
+#     category: "Spirit",
+#     image_url: "https://products1.imgix.drizly.com/ci-plymouth-gin-ad102e6133ca3c46.jpeg?auto=format%2Ccompress&fm=jpg&q=20",
+#     quantity: 34,
+#     unit: "oz"
+# )
+
+# vodka = Ingredient.create!(
+#     name: "Vodka",
+#     category: "Spirit",
+#     image_url: "https://cdn.shopify.com/s/files/1/0050/2395/7103/products/Tito_s_1L_1000x_5807f7e3-c4f8-42f1-b7fc-980938c797db_800x.jpg?v=1600907259",
+#     quantity: 34,
+#     unit: "oz"
+# )
+
+# tequila = Ingredient.create!(
+#     name: "Silver Tequila",
+#     category: "Spirit",
+#     image_url: "https://dydza6t6xitx6.cloudfront.net/ci-espolon-blanco-94fa02cf380aa76c.jpeg",
+#     quantity: 34,
+#     unit: "oz"
+# )
+
+# reposado = Ingredient.create!(
+#     name: "Reposado",
+#     category: "Spirit",
+#     image_url: "https://products0.imgix.drizly.com/ci-espolon-reposado-8ba617a1878cf0c4.jpeg?auto=format%2Ccompress&fm=jpg&q=20",
+#     quantity: 34,
+#     unit: "oz"
+# )
+
+# yellow = Ingredient.create!(
+#     name: "Yellow Chartreuse",
+#     category: "Spirit",
+#     image_url: "https://products3.imgix.drizly.com/ci-chartreuse-yellow-ca3e58dafc2955d6.jpeg?auto=format%2Ccompress&fm=jpg&q=20",
+#     quantity: 24,
+#     unit: "oz"
+# )
+
+# green = Ingredient.create!(
+#     name: "Green Chartreuse",
+#     category: "Spirit",
+#     image_url: "https://products1.imgix.drizly.com/ci-chartreuse-green-liqueur-33823a1bfd34b6d1.jpeg?auto=format%2Ccompress&fm=jpg&q=20",
+#     quantity: 24,
+#     unit: "oz"
+# )
+
+# aperol = Ingredient.create!(
+#     name: "Aperol",
+#     category: "Spirit",
+#     image_url: "https://products1.imgix.drizly.com/ci-aperol-4dbb0dd1d2e50f9d.jpeg?auto=format%2Ccompress&fm=jpg&q=20",
+#     quantity: 34,
+#     unit: "oz"
+# )
+
+# mezcal = Ingredient.create!(
+#     name: "Mezcal",
+#     category: "Spirit",
+#     image_url: "https://products3.imgix.drizly.com/ci-del-maguey-vida-mezcal-7e891b1ee7c402c7.jpeg?auto=format%2Ccompress&fm=jpg&q=20",
+#     quantity: 34,
+#     unit: "oz"
+# )
+
+# lime_juice = Ingredient.create!(
+#     name: "Lime Juice",
+#     category: "Citrus Juice",
+#     image_url: "https://dydza6t6xitx6.cloudfront.net/ci-fresh-squeezed-lime-juice-d2a4eba24e3bcff7.jpeg",
+#     quantity: 12,
+#     unit: "oz"
+# )
+
+
+
+
+
 
     # Cocktail.create!(
     #     name: "Old Fashioned",
@@ -167,61 +236,93 @@ egg_white = Ingredient.create!(
 #     running_low: false
 # )
 
-Cocktail.create!(
-    name: "Old Fashioned",
-    image_url: "https://www.liquor.com/thmb/f_Fv27Y9k7zIofk_E6rhz37BpWQ=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__liquor__2018__05__08113350__bourbon-old-fashioned-720x720-recipe-ade6f7780c304999be3577e565c9bcdd.jpg",
-    cocktail_ingredients: [
-        {
-            name: "Bourbon",
-            quantity: 2.25,
-            unit: "oz"
-        },
-        {
-            name: "Simple Syrup",
-            quantity: 0.5,
-            unit: "oz"
-        },
-        {
-            name: "Angostura Bitters",
-            quantity: 4,
-            unit: "dashes"
-        }
-    ],
-    instructions: ["measure", "shake", "strain"],
-    recommended_glass: "double rocks",
-    recommended_ice: "crushed",
-    category: "strong",
-    garnish: "orange twist"
-)
+# Cocktail.create!(
+#     name: "Old Fashioned",
+#     image_url: "https://www.liquor.com/thmb/f_Fv27Y9k7zIofk_E6rhz37BpWQ=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__liquor__2018__05__08113350__bourbon-old-fashioned-720x720-recipe-ade6f7780c304999be3577e565c9bcdd.jpg",
+#     cocktail_ingredients: [
+#         {
+#             name: "Bourbon",
+#             quantity: 2.25,
+#             unit: "oz"
+#         },
+#         {
+#             name: "Simple Syrup",
+#             quantity: 0.5,
+#             unit: "oz"
+#         },
+#         {
+#             name: "Angostura Bitters",
+#             quantity: 4,
+#             unit: "dashes"
+#         }
+#     ],
+#     instructions: ["measure", "shake", "strain"],
+#     recommended_glass: "double rocks",
+#     recommended_ice: "crushed",
+#     category: "strong",
+#     garnish: "orange twist"
+# )
+
+# Cocktail.create!(
+#     name: "Whiskey Sour",
+#     image_url: "https://dailyappetite.com/wp-content/uploads/2020/03/Bourbon-Whiskey-Sour-2.jpg",
+#     cocktail_ingredients: [
+#         {
+#             name: "Bourbon",
+#             quantity: 2,
+#             unit: "oz"
+#         },
+#         {
+#             name: "Simple Syrup",
+#             quantity: 0.75,
+#             unit: "oz"
+#         },
+#         {
+#             name: "Egg White",
+#             quantity: 1,
+#             unit: ""
+#         },
+#         {
+#             name: "Lemon Juice",
+#             quantity: 1,
+#             unit: "oz"
+#         }
+#     ],
+#     instructions: ["measure", "shake", "strain"],
+#     recommended_glass: "double rocks",
+#     recommended_ice: "crushed",
+#     category: "strong",
+#     garnish: "orange twist"
+# )
 
 Cocktail.create!(
-    name: "Whiskey Sour",
-    image_url: "https://dailyappetite.com/wp-content/uploads/2020/03/Bourbon-Whiskey-Sour-2.jpg",
+    name: "Naked and Famous",
+    image_url: "https://recipe.awesomedrinks.com/media/cocktails/naked-famous.jpg.700x700_q85_crop.jpg",
     cocktail_ingredients: [
         {
-            name: "Bourbon",
-            quantity: 2,
-            unit: "oz"
-        },
-        {
-            name: "Simple Syrup",
+            name: "Mezcal",
             quantity: 0.75,
             unit: "oz"
         },
         {
-            name: "Egg White",
-            quantity: 1,
-            unit: ""
+            name: "Yellow Chartreuse",
+            quantity: 0.75,
+            unit: "oz"
         },
         {
-            name: "Lemon Juice",
-            quantity: 1,
+            name: "Aperol",
+            quantity: 0.75,
+            unit: "oz"
+        },
+        {
+            name: "Lime Juice",
+            quantity: 0.75,
             unit: "oz"
         }
     ],
     instructions: ["measure", "shake", "strain"],
-    recommended_glass: "double rocks",
-    recommended_ice: "crushed",
-    category: "strong",
-    garnish: "orange twist"
+    recommended_glass: "Coupe",
+    recommended_ice: "none",
+    category: "Modern Classic",
+    garnish: "nope"
 )
