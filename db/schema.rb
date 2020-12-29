349bfd3d876cf833785c6244b1f8ae3dbf30a516
liamH47 <liamhanafee@gmail.com>
2020-12-29 13:29:01 -0500

    shopping list model set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_204601) do
+ActiveRecord::Schema.define(version: 2020_12_29_181527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2020_12_11_204601) do
     t.string "image_url"
     t.float "quantity"
     t.string "unit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shopping_list_items", force: :cascade do |t|
+    t.integer "ingredient_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
