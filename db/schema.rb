6052ecb2e497aaf9f40a4099c127b81964a33f03
liamH47 <liamhanafee@gmail.com>
2020-12-31 14:58:21 -0500

    auth working

--- db/schema.rb
+++ db/schema.rb
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(version: 2020_12_30_164425) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
